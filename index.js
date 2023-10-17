0580e320018c59957da464803ec47f4a241189b6
Alexis-Ali-AA-UPDS <147345317+Alexis-Ali-AA-UPDS@users.noreply.github.com>
2023-10-17 19:55:24 +0000

    cambio de archivo index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+///...Nombre Completo...///
+const nombreCompleto = (nombre) => nombre
+const nombre_Completo = nombreCompleto("Alex Alan Ali Alanoca")
+console.log("Nombre completo del estudiante es: ",nombre_Completo)
+
+///...Funcion de suma...///
+function suma (a, b){
+    return a + b
+}
+const resultado_suma = suma(2, 8)
+console.log("La suma de  2 + 8 es:",resultado_suma)
+
+///...Funcion de resta...///
+const resta = function(a, b){
+    return a - b
+}
+const resultado_resta = resta(5, 7)
+console.log("La resta de 5 - 7 es:",resultado_resta)
+
+///... Funcion de Producto...///
+const producto = (a, b) => a * b
+const resultado_producto = producto(6, 2)
+console.log("El producto de 6 * 2 es:",resultado_producto)
+
+///...Funcion de Dividir...///
+const dividir = (a, b) => a / b
+const resultado_dividir = dividir(2, 1)
+console.log("La division de 2 / 1 es:",resultado_dividir);
